868c1db5493ff16ce72c6c821ae6ff5c29a37126
Pablo <pablo_cesar1995@hotmail.com>
2021-08-09 22:50:06 -0500

    Clase 1 y 2

---
+++ 1. Creación de primera aplicación/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def mensaje():
+    return "Hola mundo"
+
+@app.get("/login")
+def mensaje():
+    return "Ingrese sus datos"
+
+@app.get("/users/{user_id}")
+def mensaje(user_id):
+    return f"El id del usuario es: {user_id}"
